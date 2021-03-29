37816635895de9821db9dc143af1055d207340de
louliakabbani <loulia.kab94@gmail.com>
2021-03-29 09:32:07 -0700

    add change

--- js/app.js
+++ js/app.js
@@ -1,80 +1,81 @@
 'use strict';
 
-
-console.log("anything");
-
 let socialSituation=prompt('are you married??');
 
  if(socialSituation==="YES")
- socialSituation===socialSituation.touppererCase();
+ socialSituation===socialSituation.toupperCase();
  {
-  console.log('socialSituation');
+ // console.log('socialSituation');
   alert('thats great to hear')}
 
-
   if(socialSituation==="NO")
-  socialSituation===socialSituation.touppererCase();{
-    console.log('socialSituation');
+  socialSituation===socialSituation.touppererCase();
+  {
+    //console.log('socialSituation');
     alert ('hope you meet your partner life soon')
   }
 
-
-
 let hairStyle=prompt('do love the short hair');
 
-  if(hairStyle===yes)
-  hairStyle===hairStyle.tolowererCase();
+  if(hairStyle==="yes")
+  hairStyle===hairStyle.tolowerCase();
   {
-      console.log('hairStyle');
+     // console.log('hairStyle');
   alert("yes,it really stylish")
 
   }
 
-
-if(hairStyle===no)
+if(hairStyle==="no")
+hairStyle===hairStyle.tolowerCase();
 {
-    console.log('hairStyle');
+   // console.log('hairStyle');
     alert('long hair is nice also')
-
 }
 
-
 let colorHair=prompt('do you prefer blond hair');
-if(colorHair===yes){
+if(colorHair==="yes")
+colorHair===colorHair.tolowerCase();{
 
-    console.log('yes,it really stylish');
+    //console.log('yes,it really stylish');
     alert("yes,it really stylish")
 }
 
-if(hairStyle===no){
-    console.log('black hair is nice also');
+if(colorHair==="no")
+colorHair===colorHair.tolowerCase();{
+
+   // console.log('black hair is nice also');
     alert('black hair is nice also')
 }
 
 
 
 let Style=prompt('do you prefer formal style');
-if( Style===yes){
-    console.log('yes,it really elegant');
+if( Style==="yes")
+Style===Style.tolowerCase();{
+   
+  //  console.log('yes,it really elegant');
     alert("yes,it really elegant")
 }
 
-if( Style===no){
-    console.log('casual style is nice also');
+if( Style==="no")
+Style===Style.tolowerCase();
+{
+    //console.log('casual style is nice also');
     alert('casual style  is nice also')
 }
 
 
 let music=prompt('do you like rap song');
-if( music===yes){
-    console.log('yes,it really cool');
+if( music==="yes")
+music===music.tolowerCase();{
+    //console.log('yes,it really cool');
     alert("yes,it really cool")
 }
 
-if( music===no){
-    console.log('jazz songis nice also');
+if( music==="no")
+music===music.tolowerCase();{
+    //console.log('jazz songis nice also');
 alert('jazz song is nice also')
 
 }
-
 
