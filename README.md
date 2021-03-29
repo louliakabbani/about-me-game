e77e8a9e52720f37e066c32299a12a4d4a48db18
louliakabbani <loulia.kab94@gmail.com>
2021-03-29 08:54:01 -0700

    do osme change

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # about-me-game
+
+#  my name is loulia kabbani:
+i'm 26 years i have a diploma in financal mangment i,m intersted in develop myselp in programing so i take this cousre
+**Links and Resources:**
+
+[github account]( https://github.com/louliakabbani/about-me-gam)
+
+How did this go, overall?
+kind of  this assighment not new overall there is some feed back information from befor so it's going with no big proplems. 
+
+
+What observations or questions do you have about what you’ve learned so far?
+ i wonder ifi can make a wesite can intract the clints with really new innovations in programing. 
+  
+How long did it take you to complete this assignment?
+ And, before you started, how long did you think it would take you to complete this assignment?
+it take me like two houre to complete it 
+but i thought it will took not much time but i face a little issues.
+
+
+
+
+copyrigth 2021
+
+
+ 
---
+++ js/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+
+console.log("anything");
+
+let socialSituation=prompt('are you married??');
+
+ if(socialSituation==="YES")
+ socialSituation===socialSituation.touppererCase();
+ {
+  console.log('socialSituation');
+  alert('thats great to hear')}
+
+
+  if(socialSituation==="NO")
+  socialSituation===socialSituation.touppererCase();{
+    console.log('socialSituation');
+    alert ('hope you meet your partner life soon')
+  }
+
+
+
+let hairStyle=prompt('do love the short hair');
+
+  if(hairStyle===yes)
+  hairStyle===hairStyle.tolowererCase();
+  {
+      console.log('hairStyle');
+  alert("yes,it really stylish")
+
+  }
+
+
+if(hairStyle===no)
+{
+    console.log('hairStyle');
+    alert('long hair is nice also')
+
+}
+
+
+let colorHair=prompt('do you prefer blond hair');
+if(colorHair===yes){
+
+    console.log('yes,it really stylish');
+    alert("yes,it really stylish")
+}
+
+if(hairStyle===no){
+    console.log('black hair is nice also');
+    alert('black hair is nice also')
+}
+
+
+
+let Style=prompt('do you prefer formal style');
+if( Style===yes){
+    console.log('yes,it really elegant');
+    alert("yes,it really elegant")
+}
+
+if( Style===no){
+    console.log('casual style is nice also');
+    alert('casual style  is nice also')
+}
+
+
+let music=prompt('do you like rap song');
+if( music===yes){
+    console.log('yes,it really cool');
+    alert("yes,it really cool")
+}
+
+if( music===no){
+    console.log('jazz songis nice also');
+alert('jazz song is nice also')
+
+}
+
+
