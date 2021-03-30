8cee7c1c4b1ecc605312eab1bbe1ea9d3b2dbadb
louliakabbani <loulia.kab94@gmail.com>
2021-03-30 10:49:32 -0700

    lab03

--- README.md
+++ README.md
@@ -6,17 +6,18 @@ i'm 26 years i have a diploma in financal mangment i,m intersted in develop myse
 
 [github account]( https://github.com/louliakabbani/about-me-gam)
 
-How did this go, overall?
-kind of  this assighment not new overall there is some feed back information from befor so it's going with no big proplems. 
+How did this go, overall?
+kind of  this assignment not new overall there is some feed back information from before so it's going with no big problems. 
 
 
-What observations or questions do you have about what you’ve learned so far?
- i wonder ifi can make a wesite can intract the clints with really new innovations in programing. 
-  
-How long did it take you to complete this assignment?
- And, before you started, how long did you think it would take you to complete this assignment?
-it take me like two houre to complete it 
-but i thought it will took not much time but i face a little issues.
+
+What observations or questions do you have about what you’ve learned so far?
+ I wonder if can make a website can interact the clients with really new innovations in programing. 
+  
+How long did it take you to complete this assignment?
+ And, before you started, how long did you think it would take you to complete this assignment?
+it take me like two hour to complete it 
+but i thought it will took not much time but i face a little issues.
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,51 +1,61 @@
 'use strict';
 
-let socialSituation=prompt('are you married??');
+let userName = prompt('whats your name?');
 
- if(socialSituation==="YES")
- socialSituation===socialSituation.toupperCase();
- {
- // console.log('socialSituation');
-  alert('thats great to hear')}
-
-  if(socialSituation==="NO")
-  socialSituation===socialSituation.touppererCase();
-  {
-    //console.log('socialSituation');
-    alert ('hope you meet your partner life soon')
-  }
-
-let hairStyle=prompt('do love the short hair');
-
-  if(hairStyle==="yes")
-  hairStyle===hairStyle.tolowerCase();
-  {
-     // console.log('hairStyle');
-  alert("yes,it really stylish")
-
-  }
-
-if(hairStyle==="no")
-hairStyle===hairStyle.tolowerCase();
-{
-   // console.log('hairStyle');
-    alert('long hair is nice also')
+while (!userName) {
+    userName = prompt('please write something');
 }
 
-let colorHair=prompt('do you prefer blond hair');
-if(colorHair==="yes")
-colorHair===colorHair.tolowerCase();{
 
-    //console.log('yes,it really stylish');
-    alert("yes,it really stylish")
-}
+alert('hello ' + userName + 'welcome to the website');
 
-if(colorHair==="no")
-colorHair===colorHair.tolowerCase();{
+// let socialSituation=prompt('are you married??');
 
-   // console.log('black hair is nice also');
-    alert('black hair is nice also')
-}
+//  if(socialSituation==="YES")
+//  socialSituation===socialSituation.toupperCase();
+//  {
+//  // console.log('socialSituation');
+//   alert('thats great to hear')}
+
+//   if(socialSituation==="NO")
+//   socialSituation===socialSituation.touppererCase();
+//   {
+//     //console.log('socialSituation');
+//     alert ('hope you meet your partner life soon')
+//   }
+
+// let hairStyle=prompt('do love the short hair');
+
+//   if(hairStyle==="yes")
+//   hairStyle===hairStyle.tolowerCase();
+//   {
+//      // console.log('hairStyle');
+//   alert("yes,it really stylish")
+
+//   }
+
+
+// if(hairStyle==="no")
+// hairStyle===hairStyle.tolowerCase();
+// {
+//    // console.log('hairStyle');
+//     alert('long hair is nice also')
+// }
+
+// let colorHair=prompt('do you prefer blond hair');
+// if(colorHair==="yes")
+// colorHair===colorHair.tolowerCase();{
+
+//     //console.log('yes,it really stylish');
+//     alert("yes,it really stylish")
+// }
+
+// if(colorHair==="no")
+// colorHair===colorHair.tolowerCase();{
+
+//    // console.log('black hair is nice also');
+//     alert('black hair is nice also')
+// }
 
 
 
@@ -57,25 +67,61 @@ Style===Style.tolowerCase();{
     alert("yes,it really elegant")
 }
 
-if( Style==="no")
-Style===Style.tolowerCase();
-{
-    //console.log('casual style is nice also');
-    alert('casual style  is nice also')
+// if( Style==="no")
+// Style===Style.tolowerCase();
+// {
+//     //console.log('casual style is nice also');
+//     alert('casual style  is nice also')
+// }
+
+
+// let music=prompt('do you like rap song');
+// if( music==="yes")
+// music===music.tolowerCase();{
+//     //console.log('yes,it really cool');
+//     alert("yes,it really cool")
+// }
+
+// if( music==="no")
+// music===music.tolowerCase();{
+//     //console.log('jazz songis nice also');
+// alert('jazz song is nice also')
+
+// }M.A.C	Check Price
+
+
+ 
+for (let i = 0; i <3; i++) {
+  let number=prompt('guess how many times i traveled to u.s.a?');
+  if(number<2 ){alert('it is too low')}
+    console.log(number);
+  if(number<2){alert('it is too low')
+  }
+   else if(number>2){
+    alert("it is too hight")
+   }
+   alert("the right answer is 2");
 }
 
 
-let music=prompt('do you like rap song');
-if( music==="yes")
-music===music.tolowerCase();{
-    //console.log('yes,it really cool');
-    alert("yes,it really cool")
+let brandsOFmakeup = ['L’Oreal','Maybelline','NYX','Avon'];
+
+for (let i = 0; i < 5; i++) 
+{ 
+  let guessBrand =prompt('guess my favorite brand of makeup?')
+  for(let j =0;j<brandsOFmakeup.length;j++){
+    if(guessBrand==brandsOFmakeup[i])
+    {
+      alert("yes you are right")
+      i=10;
+      alert('oh,no')
+
+    } 
+   
+  }
+    console.log(brandsOFmakeup [i]);
+   
 }
 
-if( music==="no")
-music===music.tolowerCase();{
-    //console.log('jazz songis nice also');
-alert('jazz song is nice also')
 
-}
 
