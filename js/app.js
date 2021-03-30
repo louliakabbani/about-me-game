b316cc1c3f7a80032eebea55a51a68d018197ad6
louliakabbani <loulia.kab94@gmail.com>
2021-03-30 11:10:30 -0700

    lab02

--- js/app.js
+++ js/app.js
@@ -9,119 +9,122 @@ while (!userName) {
 
 alert('hello ' + userName + 'welcome to the website');
 
-// let socialSituation=prompt('are you married??');
+let socialSituation=prompt('are you married??');
+socialSituation===socialSituation.toupperCase();
+ if(socialSituation==="YES")
 
-//  if(socialSituation==="YES")
-//  socialSituation===socialSituation.toupperCase();
-//  {
-//  // console.log('socialSituation');
-//   alert('thats great to hear')}
+ {
+ // console.log('socialSituation');
+  alert('thats great to hear')}
+  socialSituation===socialSituation.touppererCase();
+  if(socialSituation==="NO")
 
-//   if(socialSituation==="NO")
-//   socialSituation===socialSituation.touppererCase();
-//   {
-//     //console.log('socialSituation');
-//     alert ('hope you meet your partner life soon')
-//   }
+  {
+    //console.log('socialSituation');
+    alert ('hope you meet your partner life soon')
+  }
 
-// let hairStyle=prompt('do love the short hair');
+let hairStyle=prompt('do love the short hair');
+hairStyle===hairStyle.tolowerCase();
+  if(hairStyle==="yes")
 
-//   if(hairStyle==="yes")
-//   hairStyle===hairStyle.tolowerCase();
-//   {
-//      // console.log('hairStyle');
-//   alert("yes,it really stylish")
+  {
+     // console.log('hairStyle');
+  alert("yes,it really stylish")
 
-//   }
+  }
+
+hairStyle===hairStyle.tolowerCase();
+if(hairStyle==="no")
+
+{
+   // console.log('hairStyle');
+    alert('long hair is nice also')
+}
+
+let colorHair=prompt('do you prefer blond hair');
+colorHair===colorHair.tolowerCase();
+if(colorHair==="yes")
+
+{
+
+    //console.log('yes,it really stylish');
+    alert("yes,it really stylish")
+}
+
+if(colorHair==="no")
+colorHair===colorHair.tolowerCase();{
+
+   // console.log('black hair is nice also');
+    alert('black hair is nice also')
+}
 
 
-// if(hairStyle==="no")
-// hairStyle===hairStyle.tolowerCase();
-// {
-//    // console.log('hairStyle');
-//     alert('long hair is nice also')
-// }
-
-// let colorHair=prompt('do you prefer blond hair');
-// if(colorHair==="yes")
-// colorHair===colorHair.tolowerCase();{
-
-//     //console.log('yes,it really stylish');
-//     alert("yes,it really stylish")
-// }
-
-// if(colorHair==="no")
-// colorHair===colorHair.tolowerCase();{
-
-//    // console.log('black hair is nice also');
-//     alert('black hair is nice also')
-// }
-
-
-
+Style===Style.tolowerCase();
 let Style=prompt('do you prefer formal style');
 if( Style==="yes")
-Style===Style.tolowerCase();{
+{
    
   //  console.log('yes,it really elegant');
     alert("yes,it really elegant")
 }
+Style===Style.tolowerCase();
+if( Style==="no")
 
-// if( Style==="no")
-// Style===Style.tolowerCase();
-// {
-//     //console.log('casual style is nice also');
-//     alert('casual style  is nice also')
-// }
+{
+    //console.log('casual style is nice also');
+    alert('casual style  is nice also')
+}
 
 
-// let music=prompt('do you like rap song');
-// if( music==="yes")
-// music===music.tolowerCase();{
-//     //console.log('yes,it really cool');
-//     alert("yes,it really cool")
-// }
+let music=prompt('do you like rap song');
+music===music.tolowerCase();
+if( music==="yes")
+{
+    //console.log('yes,it really cool');
+    alert("yes,it really cool")
+}
+music===music.tolowerCase();
+if( music==="no")
+{
+    //console.log('jazz songis nice also');
+alert('jazz song is nice also')
 
-// if( music==="no")
-// music===music.tolowerCase();{
-//     //console.log('jazz songis nice also');
-// alert('jazz song is nice also')
-
-// }M.A.C	Check Price
+}
 
 
  
-for (let i = 0; i <3; i++) {
-  let number=prompt('guess how many times i traveled to u.s.a?');
-  if(number<2 ){alert('it is too low')}
-    console.log(number);
-  if(number<2){alert('it is too low')
-  }
-   else if(number>2){
-    alert("it is too hight")
-   }
-   alert("the right answer is 2");
-}
+// for (let i = 0; i <3; i++) {
+//   let number=prompt('guess how many times i traveled to u.s.a?');
+//   if(number<2 ){alert('it is too low')}
+//     console.log(number);
+//   if(number<2){alert('it is too low')
+//   }
+//    else if(number>2){
+//     alert("it is too hight")
+//    }
+//    alert("the right answer is 2");
+// }
 
 
-let brandsOFmakeup = ['L’Oreal','Maybelline','NYX','Avon'];
+// let brandsOFmakeup = ['L’Oreal','Maybelline','NYX','Avon'];
 
-for (let i = 0; i < 5; i++) 
-{ 
-  let guessBrand =prompt('guess my favorite brand of makeup?')
-  for(let j =0;j<brandsOFmakeup.length;j++){
-    if(guessBrand==brandsOFmakeup[i])
-    {
-      alert("yes you are right")
-      i=10;
-      alert('oh,no')
+// for (let i = 0; i < 5; i++) 
+// { 
+//   let guessBrand =prompt('guess my favorite brand of makeup?')
+//   for(let j =0;j<brandsOFmakeup.length;j++){
+//     if(guessBrand==brandsOFmakeup[i])
+//     {
+//       alert("yes you are right")
+//       i=10;
+//       alert('oh,no')
 
-    } 
+//     } 
    
-  }
-    console.log(brandsOFmakeup [i]);
+//   }
+//     console.log(brandsOFmakeup [i]);
    
-}
+// }
 
 
 
