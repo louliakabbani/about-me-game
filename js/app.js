9aed01d97e48c2fcd587553733be52b1153df633
yazantafesh <yazantafesh1@gmail.com>
2021-03-31 14:56:11 +0300

    removed comments

--- js/app.js
+++ js/app.js
@@ -1,155 +1,155 @@
 'use strict';
 
-// let userName = prompt('whats your name?');
+let userName = prompt('whats your name?');
 
-// while (!userName) {
-//     userName = prompt('please write something');
-// }
+while (!userName) {
+    userName = prompt('please write something');
+}
 
 
-// alert('hello ' + userName + 'welcome to the website');
+alert('hello ' + userName + 'welcome to the website');
 
-// function question1() {
+function question1() {
   
 
 
-// let socialSituation=prompt('are you married??');
-// socialSituation = socialSituation.toUpperCase();
+let socialSituation=prompt('are you married??');
+socialSituation = socialSituation.toUpperCase();
 
-//  if(socialSituation==="YES"){
+ if(socialSituation==="YES"){
  
-//  // console.log('socialSituation');
-//   alert('thats great to hear')
+ // console.log('socialSituation');
+  alert('thats great to hear')
 
-//   } else if(socialSituation==="NO"){
+  } else if(socialSituation==="NO"){
   
-//     //console.log('socialSituation');
-//     alert ('hope you meet your partner life soon')
-//   }
-// }
+    //console.log('socialSituation');
+    alert ('hope you meet your partner life soon')
+  }
+}
 
-// question1();
+question1();
 
-// function question2() {
-  
-
-
-// let hairStyle=prompt('do love the short hair');
-// hairStyle = hairStyle.toLowerCase();
-
-//   if(hairStyle==="yes")
-//   {
-//      // console.log('hairStyle');
-//   alert("yes,it really stylish")
-
-//   }
-//   else if(hairStyle==="no")
-
-
-// {
-//    // console.log('hairStyle');
-//     alert('long hair is nice also')
-// }
-
-// }
-
-// question2();
-
-// function question3() {
+function question2() {
   
 
 
-// let colorHair=prompt('do you prefer blond hair');
-// colorHair=colorHair.toLowerCase();
-// if(colorHair==="yes"){
+let hairStyle=prompt('do love the short hair');
+hairStyle = hairStyle.toLowerCase();
 
-//     //console.log('yes,it really stylish');
-//     alert("yes,it really stylish")
-// }
+  if(hairStyle==="yes")
+  {
+     // console.log('hairStyle');
+  alert("yes,it really stylish")
 
-// else if(colorHair==="no"){
+  }
+  else if(hairStyle==="no")
 
 
-//    // console.log('black hair is nice also');
-//     alert('black hair is nice also')
-// }
-// }
+{
+   // console.log('hairStyle');
+    alert('long hair is nice also')
+}
 
-// question3();
+}
 
-// function question4() {
+question2();
+
+function question3() {
   
 
 
-// let Style=prompt('do you prefer formal style');
-// Style=Style.toLowerCase();
+let colorHair=prompt('do you prefer blond hair');
+colorHair=colorHair.toLowerCase();
+if(colorHair==="yes"){
 
-// if( Style==="yes"){
+    //console.log('yes,it really stylish');
+    alert("yes,it really stylish")
+}
+
+else if(colorHair==="no"){
+
+
+   // console.log('black hair is nice also');
+    alert('black hair is nice also')
+}
+}
+
+question3();
+
+function question4() {
+  
+
+
+let Style=prompt('do you prefer formal style');
+Style=Style.toLowerCase();
+
+if( Style==="yes"){
    
-//   //  console.log('yes,it really elegant');
-//     alert("yes,it really elegant")
-// }
+  //  console.log('yes,it really elegant');
+    alert("yes,it really elegant")
+}
 
-// else if( Style==="no")
+else if( Style==="no")
 
-// {
-//     //console.log('casual style is nice also');
-//     alert('casual style  is nice also')
-// }
-// }
+{
+    //console.log('casual style is nice also');
+    alert('casual style  is nice also')
+}
+}
 
-// question4();
+question4();
 
-// function question5() {
+function question5() {
   
 
 
-// let music=prompt('do you like rap song');
-// music=music.toLowerCase();
-// if( music==="yes"){
-//     //console.log('yes,it really cool');
-//     alert("yes,it really cool")
-// }
+let music=prompt('do you like rap song');
+music=music.toLowerCase();
+if( music==="yes"){
+    //console.log('yes,it really cool');
+    alert("yes,it really cool")
+}
 
-// else if( music==="no"){
+else if( music==="no"){
 
-//     //console.log('jazz songis nice also');
-// alert('jazz song is nice also')
+    //console.log('jazz songis nice also');
+alert('jazz song is nice also')
 
-// }
-// }
+}
+}
 
-// question5();
+question5();
 
 
-// function question6() {
+function question6() {
   
 
 
-// let guessNumber =null;
+let guessNumber =null;
  
-// for (let i = 0; i <4; i++) {
-//      guessNumber=prompt('guess how many times i traveled to u.s.a?');
-//      guessNumber = parseInt(guessNumber);
+for (let i = 0; i <4; i++) {
+     guessNumber=prompt('guess how many times i traveled to u.s.a?');
+     guessNumber = parseInt(guessNumber);
 
-//      if(guessNumber<2 ){
-//        guessNumber = alert('it is too low')}
-//        else if (guessNumber>2) {
-//          guessNumber = alert ('it is too high')
-//        } else if (guessNumber === 2) {
-//          alert('Correct I traveled 2 times')
-//          break;
-//        } else {
-//          alert('please answer with a number')
-//        }
-//        if (i===3) {
-//          alert('The correct answer is 2');
-//        }
-//       }
+     if(guessNumber<2 ){
+       guessNumber = alert('it is too low')}
+       else if (guessNumber>2) {
+         guessNumber = alert ('it is too high')
+       } else if (guessNumber === 2) {
+         alert('Correct I traveled 2 times')
+         break;
+       } else {
+         alert('please answer with a number')
+       }
+       if (i===3) {
+         alert('The correct answer is 2');
+       }
+      }
   
-// }
+}
 
-// question6();
+question6();
 
 function question7() {
   
